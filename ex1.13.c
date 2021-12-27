ac74e2af8f21da092177797982a3e7a2a08ed8c1
maheswaran-linumiz <maheswaran@linumiz.com>
2021-12-27 17:45:22 +0530

    histogram length of the word

---
+++ ex1.13.c
@@ -0,0 +1,16 @@
+	// histogram length of the word//
+#include<stdio.h>
+int main()
+{
+	int c;
+	while((c=getchar())!=EOF)
+	{
+  	    if(c==' '||c=='\t'||c=='\n')
+	    {
+		    putchar('\n');
+	    }
+	    else
+		    putchar('*');
+	}
+return 0;
+}
